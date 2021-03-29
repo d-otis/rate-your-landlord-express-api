e7538fbbaefbacbf73ad9abc732058febadc1535
d-otis <dan@dan-foley.com>
2021-03-29 19:10:26 -0400

    define and export getLandlordById db query

--- db/queries.js
+++ db/queries.js
@@ -12,6 +12,18 @@ const getLandlords = (request, response) => {
   })
 }
 
+const getLandlordById = (request, response) => {
+  // console.log(typeof request.params.id)
+  const id = request.params.id
+  pool.query("SELECT * FROM landlords WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
 module.exports = {
-  getLandlords
+  getLandlords,
+  getLandlordById
 }
