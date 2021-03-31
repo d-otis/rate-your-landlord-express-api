70b3b8b4bd4bf50233441034aeeca5b0e2f5979f
d-otis <dan@dan-foley.com>
2021-03-31 11:00:18 -0400

    reformat landlords keys to be underscore_case

--- serializers/landlords.serializer.js
+++ serializers/landlords.serializer.js
@@ -2,4 +2,5 @@ const JSONAPISerializer = require('jsonapi-serializer').Serializer
 
 module.exports = new JSONAPISerializer("landlord", {
   attributes: ["id", "name", "rating", "image_url", "created_at"],
+  keyForAttribute: "underscore_case"
 })
