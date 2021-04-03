09136c97db9d534cc7967d8573022bbd1dde948d
d-otis <dan@dan-foley.com>
2021-04-03 15:36:57 -0400

    add reviews as landlord attr in JSON with it's structure

--- serializers/landlords.serializer.js
+++ serializers/landlords.serializer.js
@@ -1,11 +1,15 @@
 const JSONAPISerializer = require('jsonapi-serializer').Serializer
 
 module.exports = new JSONAPISerializer("landlord", {
-  attributes: ["id", "name", "rating", "image_url", "created_at", "properties"],
+  attributes: ["id", "name", "rating", "image_url", "created_at", "properties", "reviews"],
   keyForAttribute: "underscore_case",
   properties: {
     ref: "id",
-    attributes: ["address", "created_at", "image_url", "rating", "landlord_id"],
+    attributes: ["address", "created_at", "image_url", "rating", "landlord_id", "reviews"],
+  },
+  reviews: {
+    ref: "id",
+    attributes: ["content", "rating", "property_id"]
   },
   pluralizeType: false,
   typeForAttribute: (attribute) => {
