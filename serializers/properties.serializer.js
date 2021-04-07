62edd552ca7298d518b41471a67ed59e248064d0
d-otis <dan@dan-foley.com>
2021-04-07 19:27:18 -0400

    refine PropertySerializer JSON structure

--- serializers/properties.serializer.js
+++ serializers/properties.serializer.js
@@ -1,6 +1,21 @@
 const JSONAPISerializer = require('jsonapi-serializer').Serializer
 
 module.exports = new JSONAPISerializer("property", {
-  attributes: ["id", "address", "rating", "image_url", "landlord_id", "created_at"],
-  keyForAttribute: "underscore_case"
+  attributes: ["id", "address", "rating", "image_url", "landlord_id", "created_at", "reviews"],
+  keyForAttribute: "underscore_case",
+  reviews: {
+    ref: "id",
+    attributes: ["content", "rating", "property_id"]
+  },
+  pluralizeType: false,
+  typeForAttribute: (attribute) => {
+    switch (attribute) {
+      case "properties":
+        return "property"
+      case "reviews":
+        return "review"
+      default:
+        return undefined
+    }
+  }
 })
