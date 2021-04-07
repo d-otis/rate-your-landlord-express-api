b3964cea15925e9a0e06d816a3b1d64ef5f4775f
d-otis <dan@dan-foley.com>
2021-04-07 19:25:46 -0400

    build out query for reviews to be included with properties

--- db/queries/property.queries.js
+++ db/queries/property.queries.js
@@ -6,8 +6,25 @@ const getProperties = (request, response) => {
     if (error) {
       throw error
     }
-    const jsonapi = PropertySerializer.serialize(results.rows)
-    response.status(200).send(jsonapi)
+    const properties = results.rows
+
+    console.log(`getProperties() returning ${results.rowCount} records`)
+
+    pool.query("SELECT * FROM reviews ORDER BY created_at DESC", (error, results) => {
+      const reviews = results.rows
+      console.log(`reviews returned ${results.rowCount} records`)
+      const propertiesResult = properties.map(property => {
+        return {...property, reviews: []}
+      })
+      
+      reviews.forEach(review => {
+        const idx = propertiesResult.findIndex(property => property.id === review.property_id)
+
+        propertiesResult[idx].reviews.push(review)
+      })
+      console.log(PropertySerializer)
+      response.status(200).send(PropertySerializer.serialize(propertiesResult))
+    })
   })
 }
 
