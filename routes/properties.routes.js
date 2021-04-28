faa2857616cc30681e09c10d08cfd57b09a56665
d-otis <dan@dan-foley.com>
2021-04-28 18:14:49 -0400

    add DELETE route for properties router

--- routes/properties.routes.js
+++ routes/properties.routes.js
@@ -5,5 +5,6 @@ const db = require('../db/queries')
 router.get("/", db.getProperties)
 router.post("/", db.createProperty)
 router.patch("/:id", db.updateProperty)
+router.delete("/:id", db.deleteProperty)
 
 module.exports = router
