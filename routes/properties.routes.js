75e71b7fbbca329ab696b43727efc97f1fc715b9
d-otis <dan@dan-foley.com>
2021-05-10 18:48:51 -0400

    attach getPropertyById to show rproperty show route

--- routes/properties.routes.js
+++ routes/properties.routes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const db = require('../db/queries')
 
 router.get("/", db.getProperties)
+router.get("/:id", db.getPropertyById)
 router.post("/", db.createProperty)
 router.patch("/:id", db.updateProperty)
 router.delete("/:id", db.deleteProperty)
