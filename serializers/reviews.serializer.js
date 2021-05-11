863c6edfa085635372faa20240367d5fd94704d9
d-otis <dan@dan-foley.com>
2021-05-11 18:16:39 -0400

    add created_at/updated_at to ReviewsSerializer

--- serializers/reviews.serializer.js
+++ serializers/reviews.serializer.js
@@ -1,7 +1,7 @@
 const JSONAPISerializer = require('jsonapi-serializer').Serializer
 
 module.exports = new JSONAPISerializer("review", {
-  attributes: ["id", "content", "rating", "property_id"],
+  attributes: ["id", "content", "rating", "property_id", "created_at", "updated_at"],
   keyForAttribute: "underscore_case",
   pluralizeType: false
 })
