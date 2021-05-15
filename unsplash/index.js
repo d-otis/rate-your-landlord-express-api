775a025aa31a53892ec8a5715655bf1357bd687d
d-otis <dan@dan-foley.com>
2021-05-15 12:09:17 -0400

    add basic unsplash config

---
+++ unsplash/index.js
@@ -0,0 +1,8 @@
+const nodeFetch = require('node-fetch')
+const { createApi } = require('unsplash-js')
+const dotenv = require('dotenv').config()
+
+const unsplash = createApi({
+  accessKey: process.env.UNSPLASH_ACCESS_KEY,
+  fetch: nodeFetch
+})
