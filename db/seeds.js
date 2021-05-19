97e9b19aecc497557755f356204c91d2aec30668
d-otis <dan@dan-foley.com>
2021-05-19 14:02:06 -0400

    hello world for db seeds script

--- db/seeds.js
+++ db/seeds.js
@@ -1,1 +1,30 @@
-console.log('seeds are running')
+/* 
+1. hello world fo pg-promise
+2. #destroy_all equivalent @ top
+3. generate data echoing ruby implementation
+
+*/
+
+const pgp = require("pg-promise")({
+  capSQL: true
+})
+
+const db = pgp({ database: "rate_your_landlord_backend_development" })
+
+const landlordsColumnSet = new pgp.helpers.ColumnSet([
+  "name", "created_at", "updated_at", "image_url"
+], { table: "landlords" })
+
+const landlords = [
+  { name: "Lydia Deetz", created_at: new Date(), updated_at: new Date(), image_url: "www.google.com" }
+]
+
+const insertLandlords = pgp.helpers.insert(landlords, landlordsColumnSet)
+
+db.none(insertLandlords)
+  .then((res) => {
+    console.log('All landlords inserted')
+  })
+  .catch(err => {
+    console.log({err})
+  })
