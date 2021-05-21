a243c54e83838798475ad48335c70ad780551885
d-otis <dan@dan-foley.com>
2021-05-21 18:01:52 -0400

    add updated_at attribute in properties serializer config

--- serializers/properties.serializer.js
+++ serializers/properties.serializer.js
@@ -2,7 +2,7 @@ const JSONAPISerializer = require('jsonapi-serializer').Serializer
 const { pluralFix } = require('../db/util')
 
 module.exports = new JSONAPISerializer("property", {
-  attributes: ["id", "address", "rating", "image_url", "landlord_id", "created_at", "reviews"],
+  attributes: ["id", "address", "rating", "image_url", "landlord_id", "created_at", "updated_at" , "reviews"],
   keyForAttribute: "underscore_case",
   reviews: {
     ref: "id",
